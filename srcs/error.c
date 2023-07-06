79767fb34350453bbf7715213fc978e8473bcde4
Ikar <bguyot@student.42mulhouse.fr>
2023-07-06 10:59:31 +0200

    error function

--- srcs/error.c
+++ srcs/error.c
@@ -6,11 +6,15 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:14:18 by bguyot            #+#    #+#             */
-/*   Updated: 2023/07/06 09:14:33 by bguyot           ###   ########.fr       */
+/*   Updated: 2023/07/06 09:55:14 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/error.h"
 
 void	error(int code, t_pushswap push_swap)
-{}
+{
+	tini(push_swap);
+	ft_putstr_fd("Error\n", STDERR_FILENO);
+	exit(code);
+}
